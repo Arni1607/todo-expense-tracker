0056f1cc589acf165913659fee566150e196a4fe
Arni Tiwari <arni027btcse25@igdtuw.ac.in>
2026-09-01 19:56:25 +0530

    Add dotenv for environment based configuration

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const db = require('./db');
 const app = express();
 app.use(express.json());
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/todos', (req, res) => {
   const todos = db.prepare('SELECT * FROM todos').all();
