db38ceb174abf5e73c78aabd8135549d3ded7397
Arni Tiwari <arni027btcse25@igdtuw.ac.in>
2026-09-17 09:43:15 +0530

    Add frontend page that fetches and displays todos, enable CORS

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const db = require('./db');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
